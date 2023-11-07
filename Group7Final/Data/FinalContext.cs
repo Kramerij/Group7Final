33cdc2ab8b9965ea2d7c4a6dee7dada381274904
Danny Murray <murraydp@mail.uc.edu>
2023-11-07 15:30:40 -0500

    Added initial Dessert table data to FinalContext

--- Group7Final/Data/FinalContext.cs
+++ Group7Final/Data/FinalContext.cs
@@ -7,5 +7,17 @@ namespace Group7Final.Data
     {
         public FinalContext(DbContextOptions<FinalContext> options) : base(options) { }
         public DbSet<Dessert> Desserts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Add initial table data here:
+            modelBuilder.Entity<Dessert>().HasData(
+                new Dessert { TeamMember = "Isabelle", DessertName = "Brownie Cheesecake", DessertType = "Cheesecake", DessertTemp = "Cold" },
+                new Dessert { TeamMember = "Lai", DessertName = "Dulce De Leche Cheesecake", DessertType = "Cheesecake", DessertTemp = "Cold" },
+                new Dessert { TeamMember = "Eli", DessertName = "Maple Eclair", DessertType = "Donut", DessertTemp = "Hot" },
+                new Dessert { TeamMember = "Danny", DessertName = "Mint Chocolate Chip Malt", DessertType = "Ice Cream", DessertTemp = "Cold" },
+                new Dessert { TeamMember = "Jack", DessertName = "Rock", DessertType = "Rock", DessertTemp = "Hot" }
+            );
+        }
     }
 }
