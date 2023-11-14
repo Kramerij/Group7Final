7d1f4899009a0d9e4457fdddc76b9fc3a94c86c8
isabelle kramer <kramerij@mail.uc.edu>
2023-11-14 14:11:40 -0500

    movie.cs Added in models w/ data

---
+++ Group7Final/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace Group7Final.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string MovieName { get; set; }
+        public string TeamMember { get; set; }
+        public string Director { get; set; }
+        public int ReleaseYear { get; set; }
+    }
+}
