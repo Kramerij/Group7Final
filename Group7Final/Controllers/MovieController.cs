c0b26f0f43e7a20ce2435ae1f5287ea11d9753d8
isabelle kramer <kramerij@mail.uc.edu>
2023-11-14 14:23:18 -0500

    Added data to movie table

--- Group7Final/Controllers/MovieController.cs
+++ Group7Final/Controllers/MovieController.cs
@@ -1,9 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Group7Final.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Group7Final.Controllers
 {
     public class MovieController : Controller
     {
+        FinalContext ctx;
+        public MovieController(FinalContext context) { 
+            ctx = context;
+        }
+        [HttpGet]
+
+        [HttpGet("id")]
+
+        [HttpPost]
+
+        [HttpPut]
+
+        [HttpDelete]
         public IActionResult Index()
         {
             return View();
--- Group7Final/Data/FinalContext.cs
+++ Group7Final/Data/FinalContext.cs
@@ -7,6 +7,7 @@ namespace Group7Final.Data
     {
         public FinalContext(DbContextOptions<FinalContext> options) : base(options) { }
         public DbSet<Dessert> Desserts { get; set; }
+        public DbSet<Movie> Movies { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -18,6 +19,13 @@ namespace Group7Final.Data
                 new Dessert { TeamMember = "Danny", DessertName = "Mint Chocolate Chip Malt", DessertType = "Ice Cream", DessertTemp = "Cold" },
                 new Dessert { TeamMember = "Jack", DessertName = "Rock", DessertType = "Rock", DessertTemp = "Hot" }
             );
+            modelBuilder.Entity<Movie>().HasData(
+                new Movie {TeamMember = "Eli", MovieName = "Perfect Blue", Director = "Satoshi Kon", ReleaseYear = 1197 },
+                new Movie { TeamMember = "Lai", MovieName = "Phantom of the Opera", Director = "Joel Schomacher", ReleaseYear = 2004  },
+                new Movie { TeamMember = "Isabelle", MovieName = "Legally Blande", Director = "Robert Luketic", ReleaseYear = 2001 },
+                new Movie { TeamMember = "Danny", MovieName = "Puss In Boots: The Last Wish", Director = "Joel Crawford", ReleaseYear = 2022 },
+                new Movie { TeamMember = "Jacob", MovieName = "Avengers: Endgame", Director = "The Russo Brothers", ReleaseYear = 2019 }
+                );
         }
     }
 }
