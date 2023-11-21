26946292cc9fc74756f73190d8af7497e6528ced
Danny Murray <murraydp@mail.uc.edu>
2023-11-21 09:28:20 -0500

    Ensured Creation of Database

--- Group7Final/Data/FinalContext.cs
+++ Group7Final/Data/FinalContext.cs
@@ -11,20 +11,22 @@ namespace Group7Final.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             // Add initial table data here:
             modelBuilder.Entity<Dessert>().HasData(
-                new Dessert { TeamMember = "Isabelle", DessertName = "Brownie Cheesecake", DessertType = "Cheesecake", DessertTemp = "Cold" },
-                new Dessert { TeamMember = "Lai", DessertName = "Dulce De Leche Cheesecake", DessertType = "Cheesecake", DessertTemp = "Cold" },
-                new Dessert { TeamMember = "Eli", DessertName = "Maple Eclair", DessertType = "Donut", DessertTemp = "Hot" },
-                new Dessert { TeamMember = "Danny", DessertName = "Mint Chocolate Chip Malt", DessertType = "Ice Cream", DessertTemp = "Cold" },
-                new Dessert { TeamMember = "Jack", DessertName = "Cookies n Cream Ice Cream", DessertType = "Ice Cream", DessertTemp = "Cold" }
+                new Dessert { Id = 1, TeamMember = "Isabelle", DessertName = "Brownie Cheesecake", DessertType = "Cheesecake", DessertTemp = "Cold" },
+                new Dessert { Id = 2, TeamMember = "Lai", DessertName = "Dulce De Leche Cheesecake", DessertType = "Cheesecake", DessertTemp = "Cold" },
+                new Dessert { Id = 3, TeamMember = "Eli", DessertName = "Maple Eclair", DessertType = "Donut", DessertTemp = "Hot" },
+                new Dessert { Id = 4, TeamMember = "Danny", DessertName = "Mint Chocolate Chip Malt", DessertType = "Ice Cream", DessertTemp = "Cold" },
+                new Dessert { Id = 5, TeamMember = "Jack", DessertName = "Cookies n Cream Ice Cream", DessertType = "Ice Cream", DessertTemp = "Cold" }
             );
             modelBuilder.Entity<Movie>().HasData(
-                new Movie {TeamMember = "Eli", MovieName = "Perfect Blue", Director = "Satoshi Kon", ReleaseYear = 1197 },
-                new Movie { TeamMember = "Lai", MovieName = "Phantom of the Opera", Director = "Joel Schomacher", ReleaseYear = 2004  },
-                new Movie { TeamMember = "Isabelle", MovieName = "Legally Blande", Director = "Robert Luketic", ReleaseYear = 2001 },
-                new Movie { TeamMember = "Danny", MovieName = "Puss In Boots: The Last Wish", Director = "Joel Crawford", ReleaseYear = 2022 },
-                new Movie { TeamMember = "Jacob", MovieName = "Avengers: Endgame", Director = "The Russo Brothers", ReleaseYear = 2019 }
+                new Movie { Id = 1, TeamMember = "Eli", MovieName = "Perfect Blue", Director = "Satoshi Kon", ReleaseYear = 1197 },
+                new Movie { Id = 2, TeamMember = "Lai", MovieName = "Phantom of the Opera", Director = "Joel Schomacher", ReleaseYear = 2004 },
+                new Movie { Id = 3, TeamMember = "Isabelle", MovieName = "Legally Blande", Director = "Robert Luketic", ReleaseYear = 2001 },
+                new Movie { Id = 4, TeamMember = "Danny", MovieName = "Puss In Boots: The Last Wish", Director = "Joel Crawford", ReleaseYear = 2022 },
+                new Movie { Id = 5, TeamMember = "Jacob", MovieName = "Avengers: Endgame", Director = "The Russo Brothers", ReleaseYear = 2019 }
                 );
         }
     }
--- Group7Final/Program.cs
+++ Group7Final/Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Group7Final.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<FinalContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("Database"))
+);
+
+builder.Services.AddScoped<IDessert, DessertContext>();
+builder.Services.AddScoped<IMovieService, MovieService>();
+
 builder.Services.AddSwaggerDocument();
 
 var app = builder.Build();
@@ -12,6 +23,12 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<FinalContext>();
+    context.Database.EnsureCreated();
 }
 
 app.UseHttpsRedirection();
